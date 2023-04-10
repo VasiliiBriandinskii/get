207acdb5b1034d3e7306f060ee2f601980ae281e
Vasilii Briandinskii <Briandinskii.vn@phystech.edu>
2023-04-10 14:30:07 +0300

    part 1 completed

---
+++ task5/5-1-adc-simple.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import time as time
+GPIO.setmode(GPIO.BCM)
+
+
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
+NULL = dec2bin(0)
+
+def num2dac(dac, value):
+    signal = dec2bin(value)
+    GPIO.output(dac, signal)
+    return signal
+
+def adc(comp, dac):
+    for value in range(256):
+        signal = num2dac(dac, value)
+        voltage = value / 256 * 3.3
+        time.sleep(0.01)
+        compvalue = GPIO.input(comp)
+        if compvalue == 0:
+            print("ADC value = {:^3} -> {}, input voltage = {:.2f}".format(value, signal, voltage))
+            break
+        
+
+
+dac = [26, 19, 13, 6, 5, 11, 9, 10]
+comp = 4
+troyka = 17
+
+GPIO.setup(dac, GPIO.OUT)
+GPIO.setup(troyka, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(comp, GPIO.IN)
+
+try:
+    while True:
+        adc(comp, dac)
+        print("-")
+finally:
+    GPIO.cleanup()
